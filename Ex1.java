39100e8c54cb58188518cb90ed06ebd8ed906948
chenchulu <chenchulua@gmail.com>
2018-10-05 13:07:48 +0530

    lbl_for jenkin test

---
+++ Ex1.java
@@ -0,0 +1,18 @@
+package com.mytest;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		//Modified content in local - 8.50AM
+		System.out.print("Welcome to this world!");
+		
+		for(int cnt=1;cnt<=10;cnt++){
+			System.out.println("Hello... "+cnt);
+		}
+		
+		
+
+	}
+
+}
